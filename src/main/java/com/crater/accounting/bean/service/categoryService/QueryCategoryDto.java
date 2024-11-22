248d8e7650871303dda878796fdd87f26a4511cb
crate19970523 <s19970523s@gmail.com>
2024-11-22 09:23:49 +0800

    修正 QueryCategoryDto 及 CategoryController 中的 Boolean 參數
    將 QueryCategoryDto 及 CategoryController 中的 isActive 和 isForSaving 參數從 boolean 變更為 Boolean。這樣可以處理參數為空的情況，提升查詢及過濾功能的靈活性。

--- src/main/java/com/crater/accounting/bean/service/categoryService/QueryCategoryDto.java
+++ src/main/java/com/crater/accounting/bean/service/categoryService/QueryCategoryDto.java
@@ -1,4 +1,4 @@
 package com.crater.accounting.bean.service.categoryService;
 
-public record QueryCategoryDto(Integer serialNo, String name, boolean isActive, boolean isForSaving, String userId) {
+public record QueryCategoryDto(Integer serialNo, String name, Boolean isActive, Boolean isForSaving, String userId) {
 }
